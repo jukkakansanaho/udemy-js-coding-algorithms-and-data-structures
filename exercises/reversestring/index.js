f449c8efbe5e6abb06643393e6e221b11b81752c
Jukka Kansanaho <jukka.kansanaho@arm.com>
2019-11-30 21:46:12 +0200

    Section-3: Adding solutions, tests, pic.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,66 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function opt0(str) {
+  // Split string into an array of letters.
+  const splittedString = str.split('');
 
-module.exports = reverse;
+  // Reverse the order of letters in the Array
+  const reversedArray = splittedString.reverse();
+
+  // Join reversed array back into a string (of letters).
+  const joinedArray = reversedArray.join('');
+
+  return joinedArray;
+}
+
+// All in one line of code.
+function opt1(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+
+function opt2(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+    //debugger;
+  }
+
+  return reversed;
+}
+
+function opt3(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
+
+function opt4(str) {
+  //debugger;
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+// To run opt4() in a debugger-mode:
+// 1) add 'debugger' in the function
+// 2) add function call (e.g. opt4('abcde'))
+// 3) go to dir (where the index.js is)
+// 4) Run: node inspect index.js
+// => Debugger console opens
+// 5) Type: repl (to start interactive mode)
+// 6) Copy-paste th ecode inside the function
+// => E.g.: str.split('').reduce((rev, char) => char + rev, '');
+// REPL returns the output of the function.
+//opt4('abcde');
+//opt2('abcde');
+
+module.exports = {
+  opt0,
+  opt1,
+  opt2,
+  opt3,
+  opt4,
+};
--- exercises/reversestring/test.js
+++ exercises/reversestring/test.js
@@ -1,13 +1,66 @@
 const reverse = require('./index');
 
-test('Reverse function exists', () => {
-  expect(reverse).toBeDefined();
+// Version-0
+test('Reverse.opt0 function exists', () => {
+  expect(reverse.opt0).toBeDefined();
 });
 
-test('Reverse reverses a string', () => {
-  expect(reverse('abcd')).toEqual('dcba');
+test('Reverse.opt0 reverses a string', () => {
+  expect(reverse.opt0('abcd')).toEqual('dcba');
 });
 
-test('Reverse reverses a string', () => {
-  expect(reverse('  abcd')).toEqual('dcba  ');
+test('Reverse.opt0 reverses a string', () => {
+  expect(reverse.opt0('  abcd')).toEqual('dcba  ');
+});
+
+// Version-1
+test('Reverse.opt1 function exists', () => {
+  expect(reverse.opt1).toBeDefined();
+});
+
+test('Reverse.opt1 reverses a string', () => {
+  expect(reverse.opt1('abcd')).toEqual('dcba');
+});
+
+test('Reverse.opt1 reverses a string', () => {
+  expect(reverse.opt1('  abcd')).toEqual('dcba  ');
+});
+
+// Option-2
+test('Reverse.opt2 function exists', () => {
+  expect(reverse.opt2).toBeDefined();
+});
+
+test('Reverse.opt2 reverses a string', () => {
+  expect(reverse.opt2('abcd')).toEqual('dcba');
+});
+
+test('Reverse.opt2 reverses a string', () => {
+  expect(reverse.opt2('  abcd')).toEqual('dcba  ');
+});
+
+// Version-3
+test('Reverse.opt3 function exists', () => {
+  expect(reverse.opt3).toBeDefined();
+});
+
+test('Reverse.opt3 reverses a string', () => {
+  expect(reverse.opt3('abcd')).toEqual('dcba');
+});
+
+test('Reverse.opt3 reverses a string', () => {
+  expect(reverse.opt3('  abcd')).toEqual('dcba  ');
+});
+
+// Version-4
+test('Reverse.opt4 function exists', () => {
+  expect(reverse.opt4).toBeDefined();
+});
+
+test('Reverse.opt4 reverses a string', () => {
+  expect(reverse.opt4('abcd')).toEqual('dcba');
+});
+
+test('Reverse.opt4 reverses a string', () => {
+  expect(reverse.opt4('  abcd')).toEqual('dcba  ');
 });
