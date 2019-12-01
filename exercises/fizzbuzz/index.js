9a0ee242b65947099de217dc0d7a5f9969e15f6e
Jukka Kansanaho <jukka.kansanaho@arm.com>
2019-12-01 07:48:17 +0200

    S-7: Adding solution for S-7 problem.

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,43 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// Option-1:
+function fizzBuzz(n) {
+  for (i = 1; i <= n; i++) {
+    // Is the number multiple of 3 and 5?
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else {
+      // Is the number multiple of 3?
+      if (i % 3 === 0) {
+        console.log('fizz');
+      }
+      // Is the number multiple of 5?
+      else if (i % 5 === 0) {
+        console.log('buzz');
+      } else {
+        console.log(i);
+      }
+    }
+  }
+}
+
+// // Option-2:
+// function fizzBuzz(n) {
+//   for (i = 1; i <= n; i++) {
+//     output = '';
+//     if (i % 3 != 0 && i % 5 != 0) {
+//       console.log(i);
+//     } else {
+//       if (i % 3 === 0) {
+//         output += 'fizz';
+//       }
+//       if (i % 5 === 0) {
+//         output += 'buzz';
+//       }
+//       console.log(output);
+//     }
+//   }
+// }
 
 module.exports = fizzBuzz;
