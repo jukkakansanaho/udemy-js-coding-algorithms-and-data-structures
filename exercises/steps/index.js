84a07ba7f8d61be815b7c5ca320b8e75860d1cfa
Jukka Kansanaho <jukka.kansanaho@arm.com>
2019-12-02 20:28:21 +0200

    S-11: Adding pics + solutions for S-11 problem (steps).

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,59 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Option-0:
+// function steps(n) {
+//   const char = '#';
+//   const space = ' ';
+//
+//   for (i = 1; i <= n; i++) {
+//     let str = char.repeat(i);
+//
+//     if (i < n) {
+//       str = str + space.repeat(n - i);
+//     }
+//
+//     console.log(str);
+//   }
+// }
+
+// Option-1:
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// Option-1:
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // if (stair.length <= row) {
+  //   stair += '#';
+  // } else {
+  //   stair += ' ';
+  // }
+  // steps(n, row, stair);
+
+  // 2-liner to replace the commented code above:
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
