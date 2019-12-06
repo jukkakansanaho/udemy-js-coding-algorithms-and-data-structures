9728c840bb22b02b4c575872779876d3890b6206
Jukka Kansanaho <jukka.kansanaho@arm.com>
2019-12-06 16:58:33 +0200

    S-12: Adding solutions for S-12 problem.

--- README.md
+++ README.md
@@ -25,8 +25,23 @@ This is a study project for Udemy Coding Interview Bootcamp course by [Stephen G
 - Make sure you have nodejs newer than v8: `node -v`
 - Install Jest: `sudo npm install -g jest`
 
-## HOW to test solution
+## HOWTO: test solution code
 
 - Write your solution to the problem into `exercises/<problem_dir>/index.js`
 - Run `jest exercises/<problem_dir>/ --watch`
 - E.g. `jest exercises/palindrome/ --watch`
+
+## HOWTO: debug solution code
+
+- To run opt4() in a debugger-mode:
+  1. Add `debugger;` line in the function code (index.js).
+  2. Add function call (e.g. opt4('abcde')) to index.js outside the function
+  3. Go to dir (where the index.js is)
+  4. Run: `node inspect index.js`
+  - => Debugger console opens
+  5. Type: `repl` (to start interactive mode)
+  6. Type the variable name
+  - REPL return the current variable value
+  7. Copy-paste the code inside the function
+  - => E.g.: str.split('').reduce((rev, char) => char + rev, '');
+  - REPL returns the output of the function.
--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,61 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Option-0:
+// function pyramid(n) {
+//   for (let row = 1; row <= n; row++) {
+//     let stair = '';
+//     let columns = 2 * n - 1;
+//
+//     for (let i = 1; i <= (columns - (2 * row - 1)) / 2; i++) {
+//       stair += ' ';
+//     }
+//     for (j = 1; j <= 2 * row - 1; j++) {
+//       stair += '#';
+//     }
+//     for (let k = 1; k <= (columns - (2 * row - 1)) / 2; k++) {
+//       stair += ' ';
+//     }
+//     console.log(stair);
+//   }
+// }
 
+// Option-1:
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//
+//     for (column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// Option-2:
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+}
 module.exports = pyramid;
